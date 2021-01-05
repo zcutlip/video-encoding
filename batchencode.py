dd5a651313172e2bda5a55e23eb796448465dfcd
Zachary Cutlip <uid000@gmail.com>
2021-01-05 09:31:53 -0800

    pep8 formatting changes

--- batchencode.py
+++ batchencode.py
@@ -10,22 +10,28 @@ import tempfile
 from shutil import copyfile
 from selfcaffeinate import SelfCaffeinate
 
+
 def parse_args(argv):
-    parser=argparse.ArgumentParser()
-    parser.add_argument("--workdir",help="Directory containing video files to encode.")
-    parser.add_argument("--outdir",help="Directory to write encoded files to.")
-    parser.add_argument("--decomb",help="Optionally have Handbrake decomb video.",action="store_true")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--workdir", help="Directory containing video files to encode.")
+    parser.add_argument(
+        "--outdir", help="Directory to write encoded files to.")
+    parser.add_argument(
+        "--decomb", help="Optionally have Handbrake decomb video.", action="store_true")
     parser.add_argument(
         "--no-sleep", help="Prevent macOS from sleeping while encoding.", action="store_true")
-    args=parser.parse_args(argv)
+    args = parser.parse_args(argv)
     return args
 
+
 class BatchEncoder(object):
-    QUEUE_FILE="queue.txt"
-    def __init__(self, workdir,outdir,decomb=False):
-        self.decomb=decomb
+    QUEUE_FILE = "queue.txt"
+
+    def __init__(self, workdir, outdir, decomb=False):
+        self.decomb = decomb
         self.workdir = workdir
-        self.queue_file="%s/%s" % (self.workdir,self.QUEUE_FILE)
+        self.queue_file = "%s/%s" % (self.workdir, self.QUEUE_FILE)
         self.outdir = outdir
         self.tempdir = tempfile.mkdtemp()
         self._sanity_check_dirs()
@@ -34,7 +40,7 @@ class BatchEncoder(object):
 
     def wait(self):
         print "Running all encoders."
-        for encoder,line in self.encoders:
+        for encoder, line in self.encoders:
             encoder.run()
             encoder.wait()
             self._delete_line_from_queue(line)
@@ -49,61 +55,63 @@ class BatchEncoder(object):
         if not os.path.exists("%s" % self.queue_file):
             raise Exception("Can't find queue file: %s" % self.queue_file)
 
-
     def _process_queue_file(self):
-        self.encoders=[]
-        linecount=0
+        self.encoders = []
+        linecount = 0
         for line in open(self.queue_file).readlines():
-            linecount+=1
-            line=line.rstrip()
-            parts=line.split(',',1)
-            #handle empty or malformed line
+            linecount += 1
+            line = line.rstrip()
+            parts = line.split(',', 1)
+            # handle empty or malformed line
             if not len(parts) == 2:
-                print "Skipping line %d: %s" % (linecount,line)
+                print "Skipping line %d: %s" % (linecount, line)
                 continue
-            (input_file,output_title)=parts
-            encoder=SingleEncoder(self.workdir,self.tempdir, self.outdir,input_file,output_title,decomb=self.decomb)
-            self.encoders.append((encoder,line))
+            (input_file, output_title) = parts
+            encoder = SingleEncoder(
+                self.workdir, self.tempdir, self.outdir, input_file, output_title, decomb=self.decomb)
+            self.encoders.append((encoder, line))
 
     def _backup_queue_file(self):
-        queue_file_backup="%s.orig" % self.queue_file
-        copyfile(self.queue_file,queue_file_backup)
+        queue_file_backup = "%s.orig" % self.queue_file
+        copyfile(self.queue_file, queue_file_backup)
 
-    def _delete_line_from_queue(self,line):
-        output=[]
-        for queue_line in open(self.queue_file,"rb"):
+    def _delete_line_from_queue(self, line):
+        output = []
+        for queue_line in open(self.queue_file, "rb"):
             if line != queue_line.rstrip():
                 output.append(queue_line)
 
-        queue_out=open(self.queue_file,"wb")
+        queue_out = open(self.queue_file, "wb")
         for outline in output:
             queue_out.write(outline)
         queue_out.close()
 
 
 class SingleEncoder(object):
-    TRANSCODE="transcode-video"
-    def __init__(self, workdir,tempdir,outdir,input_file,output_title,decomb=False):
-        self.decomb=decomb
-        self.tempdir=tempdir
+    TRANSCODE = "transcode-video"
+
+    def __init__(self, workdir, tempdir, outdir, input_file, output_title, decomb=False):
+        self.decomb = decomb
+        self.tempdir = tempdir
         self.outdir = outdir
-        self.input_file=input_file
-        self.input_file_basename=os.path.basename(self.input_file)
-        #self.fq_input_file="%s/%s" % (workdir,input_file)
-        self.crops_dir="%s/%s" %(workdir,"Crops")
-        self.subtitles_dir="%s/%s" % (workdir,"subtitles")
+        self.input_file = input_file
+        self.input_file_basename = os.path.basename(self.input_file)
+        # self.fq_input_file="%s/%s" % (workdir,input_file)
+        self.crops_dir = "%s/%s" % (workdir, "Crops")
+        self.subtitles_dir = "%s/%s" % (workdir, "subtitles")
         self.output_title = output_title
-        self.outlog="%s.log" % self.input_file
-        self.fq_temp_file="%s/%s.m4v" % (self.tempdir,self.output_title)
-        self.fq_output_file="%s/%s.m4v" % (self.outdir,self.output_title)
+        self.outlog = "%s.log" % self.input_file
+        self.fq_temp_file = "%s/%s.m4v" % (self.tempdir, self.output_title)
+        self.fq_output_file = "%s/%s.m4v" % (self.outdir, self.output_title)
         self._sanity_check_dirs()
-        self.command=self._build_command()
+        self.command = self._build_command()
 
     def run(self):
         print "Running:"
         print self.command
-        self.outlog_file=open(self.outlog,"wb",0)
-        self.process=subprocess.Popen(self.command,stdout=self.outlog_file,stderr=self.outlog_file,bufsize=0)
+        self.outlog_file = open(self.outlog, "wb", 0)
+        self.process = subprocess.Popen(
+            self.command, stdout=self.outlog_file, stderr=self.outlog_file, bufsize=0)
 
     def _wait(self):
         print "Waiting for encode job of %s to complete." % self.input_file
@@ -112,7 +120,7 @@ class SingleEncoder(object):
     def wait(self):
         self._wait()
         print "Moving encoded file to %s" % self.fq_output_file
-        shutil.move(self.fq_temp_file,self.fq_output_file)
+        shutil.move(self.fq_temp_file, self.fq_output_file)
         print "Done."
 
     def _sanity_check_dirs(self):
@@ -126,10 +134,10 @@ class SingleEncoder(object):
             raise Exception("Temp directory not found: %s" % self.tempdir)
 
     def _build_command(self):
-        crop_option=self._get_crop_option()
-        subtitle_option=self._get_sub_option()
-        decomb_option=self._get_decomb_option()
-        command=[self.TRANSCODE]
+        crop_option = self._get_crop_option()
+        subtitle_option = self._get_sub_option()
+        decomb_option = self._get_decomb_option()
+        command = [self.TRANSCODE]
         if crop_option:
             for opt in crop_option:
                 command.append(opt)
@@ -145,18 +153,18 @@ class SingleEncoder(object):
         command.append(self.fq_temp_file)
         return command
 
-    def _get_sub_lang(self,srt_file_name):
-        lang=""
+    def _get_sub_lang(self, srt_file_name):
+        lang = ""
         if not srt_file_name.endswith(".srt"):
             return lang
         # "subs/mymovie.eng.srt"
-        srt_basename=os.path.basename(srt_file_name)
+        srt_basename = os.path.basename(srt_file_name)
         # "mymovie.eng.srt"
-        srt_basename=os.path.splitext(srt_basename)[0]
+        srt_basename = os.path.splitext(srt_basename)[0]
         # "mymovie.eng"
-        lang=os.path.splitext(srt_basename)[1]
+        lang = os.path.splitext(srt_basename)[1]
         # ".eng"
-        lang=lang.lstrip(".")
+        lang = lang.lstrip(".")
         # "eng"
         return lang
 
@@ -165,36 +173,38 @@ class SingleEncoder(object):
         Build option list for burning subtitles.
         Eventually this will be configurable at run-time and may return None.
         """
-        sub_opt=["--burn-subtitle","scan"]
+        sub_opt = ["--burn-subtitle", "scan"]
 
-        subtitle_glob="%s/%s.*.srt" % (self.subtitles_dir,os.path.splitext(self.input_file_basename)[0])
+        subtitle_glob = "%s/%s.*.srt" % (self.subtitles_dir,
+                                         os.path.splitext(self.input_file_basename)[0])
 
-        matching_srt_files=glob.glob(subtitle_glob)
+        matching_srt_files = glob.glob(subtitle_glob)
         for srt_file in matching_srt_files:
-            lang=self._get_sub_lang(srt_file)
-            sub_opt+=["--add-srt",srt_file]
-            sub_opt+=["--bind-srt-language",lang]
+            lang = self._get_sub_lang(srt_file)
+            sub_opt += ["--add-srt", srt_file]
+            sub_opt += ["--bind-srt-language", lang]
 
         return sub_opt
 
-
     def _get_crop_option(self):
         """build option list for cropping video."""
-        crop_file="%s/%s_crop.txt" % (self.crops_dir,self.input_file_basename)
+        crop_file = "%s/%s_crop.txt" % (self.crops_dir,
+                                        self.input_file_basename)
 
         try:
-            crop_val=open(crop_file,"rb").readline().strip()
-            crop_opt=["--crop",crop_val]
+            crop_val = open(crop_file, "rb").readline().strip()
+            crop_opt = ["--crop", crop_val]
         except Exception as e:
             print e
-            crop_opt=["--crop","detect"]
+            crop_opt = ["--crop", "detect"]
 
         return crop_opt
+
     def _get_decomb_option(self):
         """
         Do we need to set decombing?
         """
-        decomb_option=None
+        decomb_option = None
         if self.decomb:
             # From HandBrakeCLI --help:
             #    --comb-detect[=string]  Detect interlace artifacts in frames.
@@ -208,29 +218,28 @@ class SingleEncoder(object):
             #
             # -H option to transcode-video specifies option to be passed to handbrake
             # TODO: maybe enable this combo by default?
-            decomb_option=["-H","comb-detect","--filter","decomb"]
+            decomb_option = ["-H", "comb-detect", "--filter", "decomb"]
         return decomb_option
 
 
 def main():
-    args=parse_args(sys.argv[1:])
+    args = parse_args(sys.argv[1:])
 
-    decomb=args.decomb
-    workdir=args.workdir
-    outdir=args.outdir
+    decomb = args.decomb
+    workdir = args.workdir
+    outdir = args.outdir
     if args.no_sleep:
         sc = SelfCaffeinate()
     else:
         sc = None
 
-
-
     print "Creating batch encoder."
-    encoder=BatchEncoder(workdir,outdir,decomb=decomb)
+    encoder = BatchEncoder(workdir, outdir, decomb=decomb)
     print "Waiting for encoder to finish."
     encoder.wait()
     print "Batch encoder done."
     sc = None
+
 
 if __name__ == '__main__':
     main()
