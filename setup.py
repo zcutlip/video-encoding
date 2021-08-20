c275db2079ea5afd0bf77c11a4f199cf74c496e1
Zachary Cutlip <uid000@gmail.com>
2021-08-20 11:20:16 -0700

    add setup script

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+about = {}
+with open("batch_encoding/__about__.py") as fp:
+    exec(fp.read(), about)
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+setup(name='batch-video-encoding',
+      version=about["__version__"],
+      description=about["__summary__"],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="",
+      author_email="",
+      url="TBD",
+      license="MIT",
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': ['batchencode=batch_encoding.batchencode:main'], },
+      python_requires='>=3.7',
+      install_requires=[
+          'selfcaffeinate',
+          'scruffington'
+      ],
+      package_data={'sw_planet_tweets': ['config/*']},
+      )
