9efa494775e5ff42bda72f2f51a9e9be19e034db
Zachary Cutlip <uid000@gmail.com>
2022-08-09 19:20:25 -0700

    add  SingleEncoderHardware class

--- batch_encoding/single_encoder.py
+++ batch_encoding/single_encoder.py
@@ -5,13 +5,22 @@ import logging
 import os
 import shutil
 import subprocess
+import sys
 from pathlib import Path
 from typing import Dict
 
-from .command import TranscodeVideoCommand
+from .command import OtherTranscodeCommand, TranscodeVideoCommand
 from .encode_report import Encoded, EncodeReport
 
 from.exceptions import MalformedJobException
+
+
+class EncodingOptionNotSupportedException(Exception):
+    pass
+
+
+class OperatingSystemNotSupported(Exception):
+    pass
 
 
 class SingleEncoderBase:
@@ -368,3 +377,55 @@ class SingleEncoderSoftware(SingleEncoderBase):
             # TODO: maybe enable this combo by default?
             decomb_option = ["-H", "comb-detect", "--filter", "decomb"]
         return decomb_option
+
+
+class SingleEncoderHardware(SingleEncoderBase):
+    SUPPORTED_PLATFORMS = ["darwin"]
+    CROP_AUTO_ARG = "auto"
+    SUBTITLE_AUTO_ARG = "auto"
+
+    def __init__(self, tempdir, job_config: Dict, logger=None, dry_run=False, skip_encode=False):
+        if sys.platform not in self.SUPPORTED_PLATFORMS:
+            raise OperatingSystemNotSupported(
+                f"OS/platform not supported {sys.platform}")
+        super().__init__(tempdir, job_config, logger, dry_run, skip_encode)
+        if self.decomb:
+            raise EncodingOptionNotSupportedException(
+                f"--decomb option not supported for {self.__class__.__name__}")
+
+        if self.m4v:
+            raise EncodingOptionNotSupportedException(
+                f"--m4v option not supported for {self.__class__.__name__}")
+
+        if self.chapter_spec:
+            raise EncodingOptionNotSupportedException(
+                f"--chapters option not supported for {self.__class__.__name__}")
+
+    def _make_input_symlink(self):
+        input_path = Path(self.tempdir, "input")
+        input_path.mkdir(exist_ok=True)
+        fq_linkname = Path(input_path, self.output_file_base)
+        try:
+            os.symlink(self.fq_input_file, fq_linkname)
+        except FileExistsError:
+            pass
+        return fq_linkname
+
+    def _build_command(self):
+        crop_option = self._get_crop_option()
+        subtitle_option = self._get_sub_option()
+        command = OtherTranscodeCommand()
+        if crop_option:
+            for opt in crop_option:
+                command.append(opt)
+        if subtitle_option:
+            for opt in subtitle_option:
+                command.append(opt)
+        command.extend(["--hevc", "--vt", "--10-bit"])
+        self.input_file_symlink = self._make_input_symlink()
+        command.append(str(self.input_file_symlink))
+        return command
+
+    def run(self):
+        os.chdir(self.tempdir)
+        return super().run()
