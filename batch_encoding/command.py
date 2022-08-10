ddeb1a87abc832f8a34791ab95170f39d5c35f91
Zachary Cutlip <uid000@gmail.com>
2022-08-09 19:10:35 -0700

    add separate "command" classes
    - for transcode-video
    - for other-transcode

--- batch_encoding/command.py
+++ batch_encoding/command.py
@@ -2,7 +2,9 @@ import shlex
 from typing import List
 
 
-class EncodeCommand(List[str]):
+class TranscodeVideoCommand(List[str]):
+    # Don Melton's Video Transcoding project
+    # https://github.com/donmelton/video_transcoding
     TRANSCODE = "transcode-video"
 
     def __init__(self):
@@ -11,3 +13,9 @@ class EncodeCommand(List[str]):
     def __str__(self) -> str:
         command_str = shlex.join(self)
         return command_str
+
+
+class OtherTranscodeCommand(TranscodeVideoCommand):
+    # Don Melton's Other Video Transcoding project
+    # https://github.com/donmelton/other_video_transcoding
+    TRANSCODE = "other-transcode"
