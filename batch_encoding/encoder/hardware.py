28ef11203b5c3baca20699bc780367ffb5a580a2
Zachary Cutlip <uid000@gmail.com>
2023-05-12 15:26:06 -0700

    implement _get_sub_option() in SingleEncoderHardware

--- batch_encoding/encoder/hardware.py
+++ batch_encoding/encoder/hardware.py
@@ -67,6 +67,31 @@ class SingleEncoderHardware(SingleEncoderBase):
         command.append(str(self.input_file_symlink))
         return command
 
+    def _get_sub_option(self):
+        """
+        Build option list for burning subtitles.
+        Eventually this will be configurable at run-time and may return None.
+        """
+
+        # --burn-subtitle TRACK|auto
+        if self.disable_auto_burn:
+            # there is no disable-subtitle-burn for other-transcode
+            # I believe you just don't populate the "--burn-subtitle" option
+            sub_opt = []
+        elif self.burn_subtitle_num:
+            sub_opt = ["--burn-subtitle", str(self.burn_subtitle_num)]
+        else:
+            sub_opt = ["--burn-subtitle", self.SUBTITLE_AUTO_ARG]
+
+        if self.add_subtitle:
+            # --add-subtitle TRACK[=forced]|auto|all|LANGUAGE|STRING
+            sub_opt.extend(["--add-subtitle", self.add_subtitle])
+
+        # Check for external subtitle files that need to be copied
+        # other-transcode doesn't support encoding these into the resulting file
+        # so we need to copy them into place if they exist
+        self._populate_external_sub_resources()
+
     def run(self):
         os.chdir(self.tempdir)
         return super().run()
