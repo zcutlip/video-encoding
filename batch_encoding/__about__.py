d0e2547cd66b82d7d87640449b22964ecdd59ae2
Zachary Cutlip <uid000@gmail.com>
2023-08-14 18:44:05 -0700

    add VideoEncodingAbout class

--- batch_encoding/__about__.py
+++ batch_encoding/__about__.py
@@ -2,6 +2,17 @@ __title__ = "Batch Video Encoding"
 __version__ = "1.3.0"
 __summary__ = "A harness for betch-encoding DVD & Blu-Ray rips"
 
+
+class VideoEncodingAbout:
+    def __init__(self) -> None:
+        self.version = __version__
+        self.summary = __summary__
+        self.title = __title__
+
+    def __str__(self):
+        return f"{self.title.upper()}: {self.summary}. Version {self.version}"
+
+
 """
 See PEP 440 for version scheme
 https://www.python.org/dev/peps/pep-0440/#examples-of-compliant-version-schemes
