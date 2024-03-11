882347656ff7b6cdaa93868946bb63a1720b63dc
Zachary Cutlip <uid000@gmail.com>
2024-03-11 13:15:41 -0700

    add `BaseCommand` class
    - make `TranscodeVideoCommand` inherit from `BaseCommand`

--- batch_encoding/command.py
+++ batch_encoding/command.py
@@ -2,17 +2,22 @@ import shlex
 from typing import List
 
 
-class TranscodeVideoCommand(List[str]):
+class BaseCommand(List[str]):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __str__(self) -> str:
+        command_str = shlex.join(self)
+        return command_str
+
+
+class TranscodeVideoCommand(BaseCommand):
     # Don Melton's Video Transcoding project
     # https://github.com/donmelton/video_transcoding
     TRANSCODE = "transcode-video"
 
     def __init__(self):
         super().__init__([self.TRANSCODE])
-
-    def __str__(self) -> str:
-        command_str = shlex.join(self)
-        return command_str
 
 
 class OtherTranscodeCommand(TranscodeVideoCommand):
